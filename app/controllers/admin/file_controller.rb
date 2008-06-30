ca5f5d05084f77e37bc6dc8d47e6082a6adf24d0
Glenn Murray <glenn@trike.com.au>
2008-06-30 18:07:19 +1000

    Updated specs to better reflect implementation Updated controllers to better reflect MVC and OO design

--- app/controllers/admin/file_controller.rb
+++ app/controllers/admin/file_controller.rb
@@ -3,37 +3,41 @@ class Admin::FileController < ApplicationController
   
   def index
     @assets = Pathname.new(FileBrowserExtension.asset_path)
-    @asset_lock = AssetLock.lock_version
   end
   
-  def new
-    @assets = Pathname.new(FileBrowserExtension.asset_path)
-    @parent_id = params[:parent_id]
-    v = params[:v]
-    if !confirm_lock(v) and !@parent_id.nil? and @parent_id.strip != ''
-        flash[:error] = "The assets have been modified since it was last loaded. Please try again now."
-        redirect_to files_path
+  def new_file
+    if params[:type] == 'Directory'
+      @file = DirectoryAsset.new(params[:asset])
     else
-        if request.post? 
-            new_type = params[:new_type]
-            version = params[:version]   
-            if new_type == 'UPLOAD'
-               upload = params[:asset][:uploaded_data]
-               file_asset = FileAsset.create(upload, @parent_id, version)
-	       unless file_asset.success 		  
-                  flash[:error] = file_asset.errors.to_s
-	       end  
-            elsif new_type == 'CREATE'
-	       directory_name = params[:asset][:directory_name]
-               directory_asset = DirectoryAsset.create(directory_name, @parent_id, version) 
-               unless directory_asset.success
-    	          flash[:error] = directory_asset.errors.to_s           
-	       end
-            end
-            redirect_to files_path             
-        end
-        @asset_lock = AssetLock.lock_version
+      @file = FileAsset.new(params[:asset])
     end
+    
+    if @file.save!
+      redirect_to files_path
+    else
+      render new_asset_path
+    end
+  end
+  
+  def remove
+    @asset = Asset.find(params[:id])
+    @asset.destroy
+    redirect_to files_path
+  rescue ActiveRecord::RecordNotFound => error
+    flash[:error] = error.message
+    redirect_to :action => 'index'
+  end
+  
+  def edit
+    @file = Asset.find(params[:id])
+    
+    if @file.update(params[:asset])
+      flash[:notice] = @file.success
+    else
+      flash[:error] = @file.errors.full_messages.join(", ")
+    end
+    
+    redirect_to files_path
   end
   
   def children
@@ -49,78 +53,11 @@ class Admin::FileController < ApplicationController
       render :layout => false
     end
   end
-  
-  def remove
-    id = params[:id]
-    @asset_lock = AssetLock.lock_version
-    if id.nil? or id == ''
-       flash[:error] = "An error occured. Possibly the id field was not supplied." 
-       redirect_to :action => 'index' 
-    else
-       v = params[:v]
-       if !confirm_lock(v) 
-           flash[:error] = "The assets have been modified since it was last loaded. Please try again now."
-           redirect_to files_path
-       else
-          @assets = Pathname.new(FileBrowserExtension.asset_path) 
-          @path = id2path(id)       
-          if request.post?
-     	     asset_version = params[:version]      
-   	     file_dir = '' 
-	     if @path.directory?
-	     	 if DirectoryAsset.destroy(id, asset_version)    
-		    flash[:notice] = "The directory was successfully removed from the assets."      
-		 else
-		    flash[:error] = "The assets have been modified since it was last loaded hence could not be deleted." 
-		 end
-	     elsif @path.file?
-		 if FileAsset.destroy(id, asset_version)
-		    flash[:notice] = "The file was successfully removed from the assets."      
-		 else
-		    flash[:error] = "The assets have been modified since it was last loaded hence could not be deleted." 
-		 end
-	     end
-             redirect_to files_path 
-          end
-       end
-    end
-  end
-  
-  def edit
-    id = params[:id]
-    v = params[:v]
-    if !confirm_lock(v) 
-        flash[:error] = "The assets have been modified since it was last loaded. Please try again now."
-        redirect_to files_path
-    else
-        if request.post?
-          asset_version = params[:version]      
-          asset = params[:file_name]          
-          path = id2path(id)
-          if path.file?	
-               file_asset = FileAsset.update(id, asset, asset_version)	    
-               if file_asset.success		       
-                    flash[:notice] = file_asset.success
-               else
-                    flash[:error] = file_asset.errors.to_s
-               end
-          elsif path.directory?
-               dir_asset = DirectoryAsset.update(id, asset, asset_version)
-               if dir_asset.success
-   	            flash[:notice] = dir_asset.success
-               else
-                    flash[:error] = dir_asset.errors.to_s
-               end
-          end               
-          redirect_to files_path
-        end
-        @file_name = id2path(id).basename
-        @asset_lock = AssetLock.lock_version
-    end
-  end  
-  
+    
+    
   private
 
+  # move this behaviour into the AssetLock class then use above directly or in the Asset objects
   def confirm_lock(version)
       return false if (version.nil? or version.to_s.strip == '')
       current_version = AssetLock.lock_version
--- spec/models/file_asset_spec.rb
+++ spec/models/file_asset_spec.rb
@@ -12,66 +12,117 @@ def get_current_lock
     AssetLock.lock_version
 end
 
-
 describe FileAsset do
-
   before do
     @test_upload_file = 'test_image.jpg'
     @renamed_test_upload_file = 'test_image_new.jpg'
     @second_test_upload_file = 'test_image2.jpg'
     FileUtils.mkdir_p(FileBrowserExtension.asset_path)
+    @file = FileAsset.new
   end
   after do
     FileUtils.rm_r(FileBrowserExtension.asset_path)
   end
+    
+  it "should be not be valid if there are errors"
+  it "should not save the asset to the filesystem if it is invalid"
 
-  it "should create a filename" do
-    file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, get_current_lock)
-    file_asset.success.should == true
+  describe 'filesystem CRUD' do
+    it "should create a file" do
+      file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, get_current_lock)
+      file_asset.success.should == true
+    end
+
+    it "should rename a file" do
+      file_asset = FileAsset.update(path2id(full_path(@test_upload_file)), @renamed_test_upload_file, get_current_lock)
+      file_asset.success.should == "Filename has been sucessfully edited."
+    end 
+
+    it "should remove a file" do
+      FileAsset.destroy(path2id(full_path(@renamed_test_upload_file)), get_current_lock).should == true
+    end
   end
 
-  it "should rename a filename" do
-    file_asset = FileAsset.update(path2id(full_path(@test_upload_file)), @renamed_test_upload_file, get_current_lock)
-    file_asset.success.should == "Filename has been sucessfully edited."
-  end 
-
-  it "should remove a file" do
-    FileAsset.destroy(path2id(full_path(@renamed_test_upload_file)), get_current_lock).should == true
+  it 'should report the extension of the filename' do
+    @file.stub!(:name).and_return('file.jpg')
+    
+    @file.extension.should == 'jpg'
   end
 
-  it "should not create a file if it has a / \\ or leading period" do
-
+  describe 'type' do
+    it 'should identify as image if extension is png' do
+      @file.stub!(:extension).and_return('png')
+      
+      @file.image?.should be_true
+    end
+    it 'should identify as image if extension is jpg'
+    it 'should identify as image if extension is jpeg'
+    it 'should identify as image if extension is gif'
+    it 'should identify as image if extension is bmp'
+    
+    it 'should identify as image ignoring extension case' do
+      @file.stub!(:extension).and_return('PnG')
+      
+      @file.image?.should be_true
+    end
+    
+    it 'should identify as non-image if has no extension' do
+      @file.stub!(:extension).and_return('')
+      
+      @file.image?.should be_false
+    end
+    it 'should identify as non-image if extension is not recognised image type'
+  end
+  
+  describe 'embed tag' do
+    it 'should give img src if is an image' do
+      @file.stub!(:image?).and_return(true)
+      
+      @file.embed_tag.should start_with('<img src=')
+    end
+    
+    it 'should give a if not an image' do
+      @file.stub!(:image?).and_return(false)
+      
+      @file.embed_tag.should start_with('<a href=')
+    end
+    
+    it 'should point to file location in uri' do
+      @file.stub(:image?).and_return(true)
+      FileAsset.public_asset_path.stub!('assets')
+      @file.stub(:name).and_return('filename')
+      
+      @file.embed_tag.should == "<img src='assets/filename'>"
+    end
   end
 
-  it "should not upload a file if file already exists" do
-    file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, get_current_lock)
-    file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, get_current_lock)
-    file_asset.success.should == false
-    file_asset.errors.should == ["Filename already exists."]
-    FileAsset.destroy(path2id(full_path(@test_upload_file)), get_current_lock)
+  describe "name" do
+    it "should have an error if contains backslash characters"
+    it "should have an error if contains forwardslash characters"
+    it "should have an error if contains fullstop characters"
+    
+    it "should have an error if already in use" do
+      file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, get_current_lock)
+      file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, get_current_lock)
+      file_asset.success.should == false
+      file_asset.errors.should == ["Filename already exists."]
+      FileAsset.destroy(path2id(full_path(@test_upload_file)), get_current_lock)
+    end
   end
 
-  it "should not upload a file if version mismatch occurs" do
-    file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, (get_current_lock + 1))
-    file_asset.success.should == false
-    file_asset.errors.should == ["The assets have been modified since it was last loaded hence could not be created."] 
-  end
-
-  it "should not edit file if it has a / \\ or leading period" do
-
-  end
-
-
-  it "should not update if file already exists" do
-    file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, get_current_lock)
-    file_asset = FileAsset.create(fixture_file_upload(@second_test_upload_file, "image/jpg"), nil, get_current_lock)
-    id = path2id(full_path(@test_upload_file))
-    file_asset = FileAsset.update(id, @second_test_upload_file, get_current_lock)
-
-    file_asset.success.should == false
-    file_asset.errors.should == ["Filename already exists."]
-    FileAsset.destroy(path2id(full_path(@test_upload_file)), get_current_lock)    
-    FileAsset.destroy(path2id(full_path(@second_test_upload_file)), get_current_lock)    
+  describe 'on version mismatch' do
+    it 'should not upload' do
+      file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, (get_current_lock + 1))
+      file_asset.success.should == false
+      file_asset.errors.should == ["The assets have been modified since it was last loaded hence could not be created."] 
+    end
+    
+    it "should not remove" do
+      file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, get_current_lock)
+      id = path2id(full_path(@test_upload_file))
+      FileAsset.destroy(id, (get_current_lock + 1)).should == false
+      FileAsset.destroy(id, get_current_lock).should == true
+    end
   end
 
   it "should not update file if version mismatch occurs" do
@@ -80,13 +131,6 @@ describe FileAsset do
     file_asset = FileAsset.update(id, @second_test_upload_file, (get_current_lock + 1))
     file_asset.errors.should == ["The assets have been modified since it was last loaded hence could not be edited."]
     FileAsset.destroy(path2id(full_path(@test_upload_file)), get_current_lock)  
-  end
-
-  it "should not remove file if version mismatch occurs" do
-    file_asset = FileAsset.create(fixture_file_upload(@test_upload_file, "image/jpg"), nil, get_current_lock)
-    id = path2id(full_path(@test_upload_file))
-    FileAsset.destroy(id, (get_current_lock + 1)).should == false
-    FileAsset.destroy(id, get_current_lock).should == true
   end
 
 end
